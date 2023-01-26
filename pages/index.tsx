578af98a461b07a9ebee0197f5785dc92c6d6468
adamjmcgrath <adam.mcgrath@auth0.com>
2023-01-26 17:20:37 +0000

    add auth0

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const { user } = useUser();
+  console.log(user);
+
   return (
     <>
       <Head>
